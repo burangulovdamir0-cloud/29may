6a28d793d3d937d274797336044552bae4e89cb4
Damir <burangulovdamir0@gmail.com>
2026-05-31 03:20:36 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+function canFormWord($word, $segments) {
+    $a = $segments[0];
+    $b = $segments[1];
+    if ($a . $b == $word) return true;
+    if ($b . $a == $word) return true;
+    return false;
+}
+echo canFormWord('umpk', ['um', 'pk']) ? 'true' : 'false';
+echo "\n";
+echo canFormWord('umpk', ['u', 'pkm']) ? 'true' : 'false';
+echo "\n";
+echo canFormWord('umpk', ['ym', 'pk']) ? 'true' : 'false';
+?>w
